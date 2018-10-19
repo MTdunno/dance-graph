25aad350155d287c50f0ad5ba98d0e3d58e90be1
unknown <tough.matt@gmail.com>
2018-10-19 00:23:37 -0500

    setup

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => res.send('Hello World!'))
+app.get('/hello', (req,res) => {
+	res.send('Hello World!'))
+}
 
-app.listen( port, () => console.log("Express server listening on port %d in %s mode", process.env.PORT, app.settings.env) )
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen( port, () => {
+	console.log("Express server listening on port %d in %s mode", process.env.PORT, app.settings.env) 
+})
