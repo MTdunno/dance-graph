c8c08e4376d2f4dbba632b5cbf4a024bb43a92c7
unknown <tough.matt@gmail.com>
2018-10-20 15:04:10 -0500

    create view for events

--- client/src/EventList.js
+++ client/src/EventList.js
@@ -4,28 +4,29 @@ class EventList extends Component {
 	
 	componentDidMount(){
 		console.log("start mount");
-		fetch('https://quiet-reaches-88393.herokuapp.com/api/event/list').then(
-		(results) => {
-			console.log("TEST");
-			console.log(results);
-			console.log(results.json());
-			results.json();
-		}
-		,(err) => {console.log(err);console.log("TEST3");}).then(
-			data => {
-				console.log(data);
-				let danceevents = data.results.map((danceevent) => {
-					return (<div key={danceevent._id}>{danceevent.name}</div>)
-				});
-				this.setState({danceevents: danceevents});
-				console.log({danceevents: danceevents});
-			}
-		)
+//		fetch('https://quiet-reaches-88393.herokuapp.com/api/event/list').then(
+//		(results) => {
+//			console.log("TEST");
+//			console.log(results);
+//			console.log(results.json());
+//			results.json();
+//		}
+//		,(err) => {console.log(err);console.log("TEST3");}).then(
+//			data => {
+//				console.log(data);
+//				let danceevents = data.results.map((danceevent) => {
+//					return (<div key={danceevent._id}>{danceevent.name}</div>)
+//				});
+//				this.setState({danceevents: danceevents});
+//				console.log({danceevents: danceevents});
+//			}
+//		)
+		this.setState({danceevents: "DANCE EVENTS"});
 	}
 	
 	render() {
     return (<div id="container1">
-	{this.state===undefined?"oops":this.state.danceevents}
+		{this.state===undefined?"oops":this.state.danceevents}
 	</div>);
 	}
 }
