caab247ecba6b3f14557a1f24dcc0cd2f7b2f87b
unknown <tough.matt@gmail.com>
2018-10-19 22:33:51 -0500

    test dance event

--- controllers/danceevent.controller.js
+++ controllers/danceevent.controller.js
@@ -18,6 +18,8 @@ exports.danceevent_create = function (req, res) {
 
 //Simple version, without validation or sanitation
 exports.danceevent_list = function (req, res) {
-    res.send(DanceEvent.find());
+    res.send(DanceEvent.find().lean().exec(function (err, danceevents) {
+		return res.end(JSON.stringify(danceevents));
+	});
 };
 
