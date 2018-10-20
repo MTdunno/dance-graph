ed0268a9f6e97fae35e5bbe308237446fe5fd723
unknown <tough.matt@gmail.com>
2018-10-20 00:11:34 -0500

    create test data for events

---
+++ client/src/EventList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EventList extends Component {
+	render() {
+    return (
+		<input type='button' id='test'/>
+	);
+	}
+}
+
+export default EventList;
