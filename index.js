b2628046384484ba6ffdc5b07e52b5b9a28abd31
unknown <tough.matt@gmail.com>
2018-10-19 20:30:55 -0500

    test dance event

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const danceevent = require('./routes/danceevent.route) 
+const danceevent = require('./routes/danceevent.route'); 
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
