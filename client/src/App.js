47693d362439a65e52cf6a6e1bf5fdaf52f9eea2
unknown <tough.matt@gmail.com>
2018-10-25 01:45:00 -0500

    passport

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import EventList from './EventList';
 import EventForm from'./EventForm';
+import ProfileCard from './ProfileCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+		
 		  <EventForm />
           <EventList />
         </header>
