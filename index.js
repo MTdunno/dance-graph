780388c1f27ed4a6a3bf4ccf1de23d5c283f03f3
unknown <tough.matt@gmail.com>
2018-10-25 18:50:49 -0500

    passport and memcache

--- index.js
+++ index.js
@@ -84,12 +84,6 @@ passport.deserializeUser((obj, cb) => {
   cb(null, obj);
 });
 
-//STUFF FOR CORS -- GROSS
-app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
-
 app.get(
   // Login url
   '/auth/login',
@@ -164,6 +158,7 @@ app.get('/api/*', (req,res) => {
 })
 
 app.get('/', authRequired, (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 app.get('/client/css/materialize.min.css', (req, res) => {
