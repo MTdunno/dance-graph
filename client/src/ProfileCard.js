3ac1b8d56ae53be0f06ad2720bd09c723b7211c2
unknown <tough.matt@gmail.com>
2018-10-26 00:28:57 -0500

    passport and memcache

--- client/src/ProfileCard.js
+++ client/src/ProfileCard.js
@@ -12,7 +12,7 @@ class ProfileCard extends Component {
 	componentDidMount(){
 		console.log("start mount");
 		fetch('https://quiet-reaches-88393.herokuapp.com/profile/google').then(
-		response => console.log()
+		response => console.log(response)
 		,(err) => {console.log(err);console.log("TEST3");}).then(
 			data => {
 				console.log(data);
