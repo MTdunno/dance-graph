6d86eeb5ca3b13bd0b43b295e92645de683b130f
unknown <tough.matt@gmail.com>
2018-10-25 19:02:48 -0500

    passport and memcache

--- index.js
+++ index.js
@@ -114,8 +114,7 @@ app.get(
 
   // Redirect back to the original page, if any
   (req, res) => {
-	res.header("Access-Control-Allow-Origin", "*");
-    const redirect = req.session.oauth2return || '/';
+	const redirect = req.session.oauth2return || '/';
     delete req.session.oauth2return;
     res.redirect(redirect);
   }
@@ -142,6 +141,25 @@ function addTemplateVariables (req, res, next) {
   next();
 }
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 //---------PASSPORT
 
