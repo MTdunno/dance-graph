ab89edcd4ea2ffbea810b29886603150f77b8b19
unknown <tough.matt@gmail.com>
2018-10-26 20:36:04 -0500

    passport and memcache

--- client/src/ProfileCard.js
+++ client/src/ProfileCard.js
@@ -27,7 +27,7 @@ class ProfileCard extends Component {
 					</div>);
 				}
 				this.setState({profile: profile});
-			}, err => {console.log(err); console.log(response.text());}
+			}, err => {console.log(err.response);}
 		)
 		
 	}
