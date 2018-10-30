f1aba3b726b8661d71c25f058643830fe1e6ebfd
unknown <tough.matt@gmail.com>
2018-10-30 00:04:50 -0500

    cors

--- client/src/ProfileCard.js
+++ client/src/ProfileCard.js
@@ -11,7 +11,7 @@ class ProfileCard extends Component {
 	
 	componentDidMount(){
 		console.log("start mount");
-		fetch('https://quiet-reaches-88393.herokuapp.com/profile/google', {mode: 'no-cors'}).then(
+		fetch('https://quiet-reaches-88393.herokuapp.com/profile/google', {credentials: 'include'}).then(
 		response => response.json()
 		,(err) => {console.log(err);console.log("TEST3");}).then(
 			data => {
