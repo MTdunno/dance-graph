574f1ba8c229f46035fed8b0e180d4f7bb48d0fc
unknown <tough.matt@gmail.com>
2018-12-05 21:51:46 -0600

    accept headers

--- index.js
+++ index.js
@@ -165,6 +165,9 @@ app.get('/api/*', (req,res) => {
 })
 
 app.get('/', authRequired, (req, res) => {
+	res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 app.get('/client/css/materialize.min.css', (req, res) => {
