a7c1e18c5e07d29ec8450380935b15da43bbf7c3
unknown <tough.matt@gmail.com>
2018-12-05 23:05:43 -0600

    passport

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,11 +2,15 @@
 
 //Simple version, without validation or sanitation
 exports.google = (req, res) => {
+	if(req.user){
   const user = { 
     name: req.user.displayName,
     photo: req.user.image
   }
   res.json(user);
+	} else {
+		res.json({error: "must be logged in"});
+	}
 }
 
 
