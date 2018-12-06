09b3c81366c5a838bccaa6677370d0fc6a8efab0
unknown <tough.matt@gmail.com>
2018-12-05 23:51:29 -0600

    passport

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ app.get('/api/*', (req,res) => {
 	res.send("This is the generic API page");
 })
 
-app.get('/', authRequired, (req, res) => {
+app.get('/', passport.authenticate('google', { scope: ['email', 'profile'] }), (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 app.get('/client/css/materialize.min.css', (req, res) => {
