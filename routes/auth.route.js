93800f2b091f9dd4f892ef78ab9a0060f8f1ad6c
unknown <tough.matt@gmail.com>
2018-12-05 22:57:07 -0600

    passport

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,8 +7,9 @@ const auth_controller = require('../controllers/auth.controller');
 function authRequired (req, res, next) {
   console.log(req.user);
   if (!req.user) {
-	  console.log("RETURN URL")
+	  console.log("RETURN URL");
 	  console.log(req.originalUrl);
+	  console.log(req);
     req.session.oauth2return = req.originalUrl;
     return res.redirect('/auth/login');
   }
