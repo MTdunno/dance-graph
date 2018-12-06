d68ee8a053210732d4b61ffff284f35414e61a6a
unknown <tough.matt@gmail.com>
2018-12-05 23:31:01 -0600

    passport

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,19 +4,19 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const auth_controller = require('../controllers/auth.controller');
 
-//function authRequired (req, res, next) {
-//  console.log(req.user);
-//  if (!req.user) {
-//	  console.log("RETURN URL");
-//	  console.log(req.originalUrl);
-//	  console.log(req);
-//    req.session.oauth2return = req.originalUrl;
-//    return res.redirect('/auth/login');
-//  }
-//  next();
-//}
+function authRequired (req, res, next) {
+  console.log(req.user);
+  if (!req.user) {
+	  console.log("RETURN URL");
+	  console.log(req.originalUrl);
+	  console.log(req);
+    req.session.oauth2return = req.originalUrl;
+    return res.redirect('/auth/login');
+  }
+  next();
+}
 
 
 // a simple test url to check that all of our files are communicating correctly.
-router.get('/google', auth_controller.google);
+router.get('/google',authRequired, auth_controller.google);
 module.exports = router;
